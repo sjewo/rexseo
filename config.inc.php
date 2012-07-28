15f5b4d502b7c1469f12055c489aca1524f12385
jdlx <github@rexdev.de>
2012-07-28 12:30:06 +0200

    moving code & funcs - part 1

--- config.inc.php
+++ config.inc.php
@@ -86,56 +86,6 @@ Disallow:',
 // --- /DYN
 
 
-// RUN CACHER ON DB CHANGES
-////////////////////////////////////////////////////////////////////////////////
-if ($REX['REDAXO'])
-{
-  rex_register_extension('REX_FORM_SAVED','rexseo_ht_update_callback');
-  function rexseo_ht_update_callback($params)
-  {
-    rexseo_htaccess_update_redirects();                                         #FB::log($params,__FUNCTION__.' $params');
-  }
-}
-
-
-// AUTO CREATE REDIRECTS FROM CHANGED URLS
-////////////////////////////////////////////////////////////////////////////////
-if ($REX['REDAXO'] && $REX['MOD_REWRITE'] !== false && $REX['ADDON'][$myself]['settings']['auto_redirects']!=0)
-{
-  rex_register_extension('REXSEO_PATHLIST_BEFORE_REBUILD','rexseo_remember_prior_pathlist');
-  function rexseo_remember_prior_pathlist($params)
-  {
-    global $REX;
-    $REX['REXSEO_PRIOR_URLS'] = $params['subject']['REXSEO_URLS'];
-  }
-
-  rex_register_extension('REXSEO_PATHLIST_FINAL','rexseo_auto_301');
-  function rexseo_auto_301($params)
-  {
-    global $REX;
-
-    $diff = array();
-    $diff = array_diff(array_keys($REX['REXSEO_PRIOR_URLS']),array_keys($params['subject']['REXSEO_URLS']));
-
-    if(is_array($diff) && count($diff)>0)
-    {
-      $db = new rex_sql;
-      $qry = 'INSERT INTO `'.$REX['TABLE_PREFIX'].'rexseo_redirects` (`id`, `createdate`, `updatedate`, `expiredate`, `creator`, `status`, `from_url`, `to_article_id`, `to_clang`, `http_status`) VALUES';
-      $date = time();
-      $expire = $date + ($REX['ADDON']['rexseo']['settings']['default_redirect_expire']*24*60*60);
-      $status = $REX['ADDON']['rexseo']['settings']['auto_redirects']==1 ? 1 : 0;
-      foreach($diff as $k=>$url)
-      {
-        $qry .= PHP_EOL.'(\'\', \''.$date.'\', \''.$date.'\', \''.$expire.'\', \'rexseo\', '.$status.', \''.$url.'\', '.$REX['REXSEO_PRIOR_URLS'][$url]['id'].', '.$REX['REXSEO_PRIOR_URLS'][$url]['clang'].', 301),';
-      }
-      $qry = rtrim($qry,',').';';
-      $db->setQuery($qry);
-      rexseo_htaccess_update_redirects();
-    }
-  }
-}
-
-
 // RUN ON ADDONS INLCUDED
 ////////////////////////////////////////////////////////////////////////////////
 rex_register_extension('ADDONS_INCLUDED','rexseo_init');
