7423196462c0fdf4ef217c1ae19e5a1cadb80ca7
jdlx <github@rexdev.de>
2012-12-06 00:59:54 +0100

    version bump -> 1.5.2

--- config.inc.php
+++ config.inc.php
@@ -11,7 +11,7 @@
  * @author markus.staab[at]redaxo[dot]de Markus Staab
  *
  * @package redaxo 4.3.x/4.4.x
- * @version 1.5.1
+ * @version 1.5.2
  */
 
 // ADDON PARAMS
@@ -23,7 +23,7 @@ $REX['ADDON'][$myself]['VERSION'] = array
 (
 'VERSION'      => 1,
 'MINORVERSION' => 5,
-'SUBVERSION'   => 1,
+'SUBVERSION'   => 2,
 );
 
 $REX['ADDON']['rxid'][$myself]        = '750';
