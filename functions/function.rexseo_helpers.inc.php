6781927a93b57e587bc3bcb14a573c8507bcead3
jdlx <github@rexdev.de>
2012-12-27 02:10:56 +0100

    version bump -> 1.5.3 nightly 27.12.2012

--- functions/function.rexseo_helpers.inc.php
+++ functions/function.rexseo_helpers.inc.php
@@ -11,7 +11,7 @@
  * @author markus.staab[at]redaxo[dot]de Markus Staab
  *
  * @package redaxo 4.3.x/4.4.x
- * @version 1.5.3 nightly b044cd6
+ * @version 1.5.3 nightly 27.12.2012
  */
 
 if (!function_exists('rexseo_recursive_copy'))
